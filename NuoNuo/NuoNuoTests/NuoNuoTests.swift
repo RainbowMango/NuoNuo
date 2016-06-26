a65acb3799776a9418f8e028c1783e88c26a4ace
RainbowMango <qdurenhongcai@163.com>
2016-06-26 15:51:36 +0800

    Initial project.

---
+++ NuoNuo/NuoNuoTests/NuoNuoTests.swift
@@ -0,0 +1,36 @@
+//
+//  NuoNuoTests.swift
+//  NuoNuoTests
+//
+//  Created by ruby on 16/6/26.
+//  Copyright © 2016年 ruby. All rights reserved.
+//
+
+import XCTest
+@testable import NuoNuo
+
+class NuoNuoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
