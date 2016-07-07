a3b37d4a5385ffdace48c534a09965edbf15eb85
RainbowMango <qdurenhongcai@163.com>
2016-07-07 20:51:07 +0800

    Add bridging file Add NetworkResourceDef file

--- NuoNuo/NuoNuo/AppDelegate.swift
+++ NuoNuo/NuoNuo/AppDelegate.swift
@@ -16,7 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        Bmob.registerWithAppKey(BMOB_APP_KEY)
+        
         return true
     }
 
