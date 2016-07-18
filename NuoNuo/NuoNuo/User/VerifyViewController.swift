4b3b7d556d42406eb866c27205995ee45b2bb90f
RainbowMango <qdurenhongcai@163.com>
2016-07-18 23:37:49 +0800

    Add verify view controller for collect identify information.

---
+++ NuoNuo/NuoNuo/User/VerifyViewController.swift
@@ -0,0 +1,43 @@
+//
+//  VerifyViewController.swift
+//  NuoNuo
+//
+//  Created by ruby on 16/7/18.
+//  Copyright © 2016年 ruby. All rights reserved.
+//
+
+import UIKit
+
+class VerifyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /**
+     隐藏状态栏
+     
+     - returns: bool
+     */
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
