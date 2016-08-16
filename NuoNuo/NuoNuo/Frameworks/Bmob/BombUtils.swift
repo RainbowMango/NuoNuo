af96aa6733f31736a122e2bcf78fc232cbbdfbab
RainbowMango <qdurenhongcai@163.com>
2016-08-16 23:38:09 +0800

    Add user avatar function debug finished.

--- NuoNuo/NuoNuo/Frameworks/Bmob/BombUtils.swift
+++ NuoNuo/NuoNuo/Frameworks/Bmob/BombUtils.swift
@@ -8,6 +8,12 @@
 
 import Foundation
 
+/// 用户头像表
+let BOMB_USER_AVATAR_TABLE = "userAvatar"
+let BOMB_USER_AVATAR_COLUMN_KEY = "mobilePhoneNumber"
+let BOMB_USER_AVATAR_COLUMN_URL = "avatarURL"
+
+
 /**
  检查手机号是否已经注册
  
@@ -38,20 +44,70 @@ func isNickNameReserved(name: String, result: ((reserved: Bool) -> Void)) -> Voi
     }
 }
 
-func uploadAvatar(image: UIImage, phone: String, result: ((url: String) -> Void)) -> Void {
+/**
+ 查询用户头像是否已经存在
+ 
+ - parameter phone:  用户手机号
+ - parameter result: 查询结果回调函数
+ */
+func isAvatarExist(phone: String, result: ((exist: Bool) -> Void)) -> Void {
+    let bquery = BmobQuery(className: BOMB_USER_AVATAR_TABLE)
+    bquery.whereKey(BOMB_USER_AVATAR_COLUMN_KEY, equalTo: phone)
+    
+    bquery.findObjectsInBackgroundWithBlock { (array, error) in
+        result(exist: !array.isEmpty)
+    }
+}
+
+/**
+ 创建用户头像，适用于首次添加用户头像
+ 
+ - parameter image:  用户头像
+ - parameter phone:  用户手机号，将用户数据表主键
+ - parameter result: 添加结果回调函数，添加成功返回文件url，失败返回空
+ */
+func bombAddAvatar(image: UIImage, phone: String, result: ((url: String) -> Void)) -> Void {
     let imageData = ImageHandler().getImageBinary(image, compressionQuality: 1.0)
     if(imageData.data == nil) {
+        print("bombAddAvatar: 转换图片失败")
         return
     }
     let imageName = phone + "." + imageData.mine!
     
-    //let obj = BmobObject(className: "_User")
+    let obj = BmobObject(className: BOMB_USER_AVATAR_TABLE)
     let bombFile = BmobFile(fileName: imageName, withFileData: imageData.data)
     
     bombFile.saveInBackground { (successful, error) in
         if(successful) {
-            print("上传成功： \(bombFile.url)")
-            result(url: bombFile.url)
+            obj.setObject(phone, forKey: BOMB_USER_AVATAR_COLUMN_KEY)
+            obj.setObject(bombFile.url, forKey: BOMB_USER_AVATAR_COLUMN_URL)
+            obj.saveInBackgroundWithResultBlock({ (addSuccess, addError) in
+                if(addError != nil) {
+                    print("bombAddAvatar: 添加数据失败: \(addError.localizedDescription)")
+                    result(url: String())
+                    return
+                }
+                result(url: bombFile.url)
+            })
         }
     }
+}
+
+func bombUpdateAvatar() -> Void {
+    
+}
+
+func uploadAvatar(image: UIImage, phone: String, result: ((url: String) -> Void)) -> Void {
+    
+    isAvatarExist(phone) { (exist) in
+        if(!exist) {
+            //新用户首次添加头像
+            bombAddAvatar(image, phone: phone, result: result)
+        }
+        
+        //更新用户头像
+        
+        //删除用户原头像文件
+    }
+    
 }
