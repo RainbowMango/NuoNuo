2794f6a71f5a9fabd5cb281d0d61c101d604ff82
RainbowMango <qdurenhongcai@163.com>
2016-10-10 21:51:04 +0800

    add resource file

---
+++ NuoNuo/NuoNuo/NetworkResourceDef.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkResourceDef.swift
+//  NuoNuo
+//
+//  Created by ruby on 16/7/7.
+//  Copyright © 2016年 ruby. All rights reserved.
+//
+
+import Foundation
+
+let BMOB_APP_KEY = "3e11d9ca1dbc1830bbd79f28ca246e58"
+
+//MOB短信验证相关
+let MOB_APP_KEY    = "159505c689d4a"
+let MOB_APP_SECRET = "a9010c3347d67b9c0c282cf6110fcf79"
+
+//测试账号
+let ADMIN_PHONE    = "18605811857"
+let ADMIN_CODE     = "1857"
